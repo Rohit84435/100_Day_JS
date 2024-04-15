1d9c157a58148d3321efb74a9ca5f36480609510
Rohit84435 <rohit349a@gmail.com>
2024-04-15 20:15:59 +0530

    eight commit

---
+++ Day8.js
@@ -0,0 +1,23 @@
+// Factorial of a number
+
+// Solution - 1
+// const fact = (num) => {
+//   if (num <= 1) return num;
+
+//   return num * fact(num - 1);
+// };
+
+// Solution - 2
+
+const fact = (num) => {
+  let ans = 1;
+  while (num > 0) {
+    ans *= num;
+    num--;
+  }
+  return ans;
+};
+
+console.log(fact(5));
+console.log(fact(8));
+console.log(fact(6));
