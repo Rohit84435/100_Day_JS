678546439ebc74d6080f0c8c168f4fbbed4df741
Rohit84435 <rohit349a@gmail.com>
2024-04-15 20:06:44 +0530

    Day-7

---
+++ Day7.js
@@ -0,0 +1,21 @@
+// Write a function to find maximum number in an array
+
+// Solution - 1
+const findMax = (arr) => {
+  let maxi = arr.reduce((acc, cur) => {
+    if (acc < cur) acc = cur;
+
+    return acc;
+  }, -99999);
+
+  return maxi;
+};
+
+// Solution - 2
+const findMax1 = (arr) => {
+  return Math.max(...arr);
+};
+
+console.log(findMax1([1, 5, 3, 9, 2]));
+console.log(findMax1([-10, -5, -3, -9, -2]));
+console.log(findMax1([5]));
