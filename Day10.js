1aa942c36409708eee15083fed6c2f09681812c5
Rohit84435 <rohit349a@gmail.com>
2024-04-16 14:19:55 +0530

    tenth commit

---
+++ Day10.js
@@ -0,0 +1,12 @@
+// Write a function arraysAreEqual that takes two arrays arr1 and arr2 as input and returns true if the
+// the array  are equal
+
+const arrayAreEqual = (arr1, arr2) => {
+  n1 = arr1.length;
+  n2 = arr2.length;
+  if (n1 != n2) return false;
+  return arr1.every((curVal, idx) => curVal === arr2[idx]);
+};
+console.log(arrayAreEqual([1, 2, 3], [1, 2, 3]));
+console.log(arrayAreEqual([1, 2, 3], [1, 2, 4]));
+console.log(arrayAreEqual([], []));
