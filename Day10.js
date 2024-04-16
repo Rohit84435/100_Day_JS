b5094c65853e457513464fd623f87c383b32d418
Rohit84435 <rohit349a@gmail.com>
2024-04-16 14:31:40 +0530

    tenth commit

--- Day10.js
+++ Day10.js
@@ -1,6 +1,8 @@
 // Write a function arraysAreEqual that takes two arrays arr1 and arr2 as input and returns true if the
 // the array  are equal
 
+// We use every((currentValue,index)=>{})
+
 const arrayAreEqual = (arr1, arr2) => {
   n1 = arr1.length;
   n2 = arr2.length;
